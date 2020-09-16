51c7c818cb1ba6093400e370dae8806976a8a67d
humorous-storage-7521 <rmwerner@umich.edu>
2020-09-16 16:54:35 -0500

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,27 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {message: ""};
+  }
+
+  changeHandler = (event) => {
+    this.setState({
+      message: event.target.value
+    }, console.log(this.state))
+  }
+
+  displayCharsLeft = () => {
+    let charsLeft = this.props.maxChars - this.state.message.length
+    console.log(charsLeft)
+    return <span>{charsLeft}</span>
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.changeHandler} value={this.state.message} />
+        {this.displayCharsLeft()}
       </div>
     );
   }
