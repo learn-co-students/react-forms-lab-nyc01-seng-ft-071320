c4824b97fb5973569736c03a935682d3b018b6e6
DBenson1017 <devinbenson@gmail.com>
2020-09-17 08:44:29 -0400

    controlled input

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
+
+  changeHandler=(e)=> {
+    this.setState( {message: e.target.value} )
+  }
+
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={this.changeHandler} placeholder={this.state.message.length} />
+        <span> Characters left: {(280 - this.state.message.length)} </span> 
       </div>
     );
   }
