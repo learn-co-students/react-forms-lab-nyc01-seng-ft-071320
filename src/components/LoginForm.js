c0a0479cc6fb817084ec39c44528a3ea5ce88245
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-09-17 01:51:06 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,38 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  loginHandler = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+
+    if (!this.state.username || !this.state.password) return 
+    this.props.handleLogin(this.state)
+    this.setState({username: "", password: ""})
   }
 
   render() {
+    console.log('my state:', this.state)
     return (
-      <form>
+      <form onSubmit={this.submitHandler}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.loginHandler}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.loginHandler}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,27 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  changeHandler = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+    console.log(this.state.message)
+  }
+
+  countChars = (value) => {
+    return <span>{this.props.maxChars - value.length} remaining characters left</span>
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={this.changeHandler} />
+        <br></br>
+        {this.countChars(this.state.message)}
       </div>
     );
   }
